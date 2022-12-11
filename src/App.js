e424a78ddd8fc2d88ad1a091de00424e4bc7984e
Federico Peluffo <fedepel@gmail.com>
2022-12-11 19:52:19 -0300

    primer subida

---
+++ src/App.js
@@ -0,0 +1,42 @@
+function Header({ numberOfProducts }) {
+  return (
+    <header> This is my header number of products {numberOfProducts}</header>
+  );
+}
+
+function Product({ product }) {
+  return (
+    <div key={product.id}>
+      {product.name}
+      {product.price}
+    </div>
+  );
+}
+
+function Store() {
+  const products = [
+    {
+      id: 1,
+      name: "Mani",
+      price: "$1US"
+    }
+  ];
+  return (
+    <main>
+      {products.map((product) => (
+        <Product product={product} />
+      ))}
+    </main>
+  );
+}
+
+export default function App() {
+  const numberOfProducts = 0;
+  return (
+    <div className="App">
+      <Header numberOfProducts={numberOfProducts}></Header>
+      <h1>Hello CodeSandbox {numberOfProducts}</h1>
+      <Store />
+    </div>
+  );
+}
